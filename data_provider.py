4305f0ebf167df870fe3a60d61f05d94c482e008
dkollias <dkollias1@gmail.com>
2016-09-29 12:01:40 +0100

    Update data_provider.py

--- data_provider.py
+++ data_provider.py
@@ -5,15 +5,18 @@ from __future__ import print_function
 import os
 import tensorflow as tf
 
-from datasets import dataset_utils
+from models.slim.datasets import dataset_utils
+import pdb 
 
 slim = tf.contrib.slim
 
 _ITEMS_TO_DESCRIPTIONS = {
-    'image': 'A [28 x 28 x 1] grayscale image.',
-    'label': 'A single integer between 0 and 9',
+    'image': 'a frame from RECOLA database',
+    'audio': 'the corresponding audio',
+    'label': 'the label for this frame',
 }
 
+_SPLITS_TO_SIZES = {'train': 16, 'test': 15}
 
 def get_split(split_name, dataset_dir, file_pattern='%s/*.tfrecords'):
   """Gets a dataset tuple with instructions for reading MNIST.
@@ -29,7 +32,8 @@ def get_split(split_name, dataset_dir, file_pattern='%s/*.tfrecords'):
   Raises:
     ValueError: if `split_name` is not a valid train/test split.
   """
-  if split_name not in 'train:
+
+  if split_name not in 'train':
     raise ValueError('split name %s was not recognized.' % split_name)
 
 
@@ -38,15 +42,18 @@ def get_split(split_name, dataset_dir, file_pattern='%s/*.tfrecords'):
   reader = tf.TFRecordReader
 
   keys_to_features = {
+      #'frame': tf.FixedLenFeature((), tf.string, default_value=''),
       'image/encoded': tf.FixedLenFeature((), tf.string, default_value=''),
-      'image/format': tf.FixedLenFeature((), tf.string, default_value='raw'),
-      'image/class/label': tf.FixedLenFeature(
-          [1], tf.int64, default_value=tf.zeros([1], dtype=tf.int64)),
+      'image/format': tf.FixedLenFeature((), tf.string, default_value='jpg'),
+      'raw_audio': tf.FixedLenFeature([150,640], tf.float32, default_value=tf.zeros([150,640], dtype=tf.float32)),
+      'label': tf.FixedLenFeature(
+          [2], tf.float32, default_value=tf.zeros([2], dtype=tf.float32)),
   }
 
   items_to_handlers = {
-      'image': slim.tfexample_decoder.Image(shape=[28, 28, 1], channels=1),
-      'label': slim.tfexample_decoder.Tensor('image/class/label', shape=[]),
+      'image': slim.tfexample_decoder.Image(shape=[28, 28], channels=1),
+      'audio': slim.tfexample_decoder.Tensor('raw_audio'),
+      'label': slim.tfexample_decoder.Tensor('label'),
   }
 
   decoder = slim.tfexample_decoder.TFExampleDecoder(
@@ -55,6 +62,7 @@ def get_split(split_name, dataset_dir, file_pattern='%s/*.tfrecords'):
   labels_to_names = None
   if dataset_utils.has_labels(dataset_dir):
     labels_to_names = dataset_utils.read_label_file(dataset_dir)
+  
 
   return slim.dataset.Dataset(
       data_sources=file_pattern,
@@ -66,6 +74,33 @@ def get_split(split_name, dataset_dir, file_pattern='%s/*.tfrecords'):
 
 
 def get_data(portion='train', batch_size=32):
+    #root = '/vol/atlas/homes/gt108/db/RECOLA_CNN/wav_data/{}*.wav'.format(portion)
+    #filename = tf.matching_files(root)
+    #contents = tf.read_file(filename)
+    #sampled_audio = tf.audio.decode_audio(
+    #        contents, file_format='wav', samples_per_second=16000, channel_count=1)
+
+
+    #tf.train.batch(tensors, batch_size, num_threads=1, capacity=batch_size*16)
+
+    dataset = get_split('train','/vol/atlas/homes/gt108/db/RECOLA_CNN/tf_records/' )
+    data_provider = slim.dataset_data_provider.DatasetDataProvider(dataset,shuffle=True)
+    [image, label, audio] = data_provider.get(['image', 'label','audio'])
+    images,labels,audios = tf.train.batch([image, label, audio], batch_size, num_threads=1, capacity=batch_size*16)
+    pdb.set_trace()
+
+    return images,labels,audios
+
+
+def main(_):
+    get_data()
+
+
+if __name__ == '__main__':
+    tf.app.run()
+
+'''
+def get_data(portion='train', batch_size=32):
     root = '/vol/atlas/homes/gt108/db/RECOLA_CNN/wav_data/{}*.wav'.format(portion)
     filename = tf.matching_files(root)
     contents = tf.read_file(filename)
@@ -74,4 +109,4 @@ def get_data(portion='train', batch_size=32):
 
 
     tf.train.batch(tensors, batch_size, num_threads=1, capacity=batch_size*16)
-
+'''
